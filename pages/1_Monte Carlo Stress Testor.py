00d7bbd64a8b36808536543e41dbe311b14abfed
Tharun-Raviendran <tharun.raviendran@gmail.com>
2024-09-02 22:13:52 -0400

    Changed clases within the page file

--- pages/1_Monte Carlo Stress Testor.py
+++ pages/1_Monte Carlo Stress Testor.py
@@ -1,10 +1,131 @@
 import sys
 import os
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+import yfinance as yf
+from scipy.stats import linregress
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+class MonteCarlo:
 
-from my_app.classes.monte_carlo_class import MonteCarlo
+    def __init__(self, user_stocks_input, user_shock, annual_risk_free_rate):
+        self.user_stocks_input = user_stocks_input
+        self.user_shock = user_shock
+        self.annual_risk_free_rate = annual_risk_free_rate
+        self.sp500 = None
+        # self.bootstraped_sp500 = []
+        self.user_stocks_str = []
+        self.user_stocks = []
+        self.betas_for_portfolio = []
+        # self.weights = []
+        self.mc_port_returns = []
+        self.mc_port_vol = []
+        self.mc_port_weights = []
+        self.mc_sharpe_ratios = []
+
+    def create_user_stocks_str (self):
+        self.user_stocks_str = self.user_stocks_input.strip(" ").split(" ")
+    
+    def create_user_stocks(self):
+        self.user_stocks = []
+        for stock in self.user_stocks_str:
+            try:
+                data = yf.download(stock, start='2006-01-01')
+                if data.empty:
+                    raise ValueError(f"No data found for stock: {stock}")
+                self.user_stocks.append(data)
+            except Exception as e:
+                raise ValueError(f"Error downloading data for stock {stock}: {e}")
+
+        for stock in self.user_stocks:
+            stock['Log Returns'] = np.log(stock['Adj Close'] / stock['Adj Close'].shift(1))
+            stock.dropna(inplace=True)    
+
+
+    # def create_user_stocks (self):
+    #     for stock in self.user_stocks_str:
+    #         data = yf.download(stock, start='2006-01-01')
+    #         self.user_stocks.append(data)
+
+    #     for stock in self.user_stocks:
+    #         stock['Log Returns'] = np.log(stock['Adj Close']/stock['Adj Close'].shift(1))
+    #         stock.dropna(inplace=True)
+
+    def create_sp500 (self):
+        self.sp500 = yf.download("^GSPC", start = '2006-01-01')
+        self.sp500['Log Returns'] = np.log(self.sp500['Adj Close']/self.sp500['Adj Close'].shift(1))
+        self.sp500.dropna(inplace=True)
+
+    def create_betas_for_portfolio(self):
+        for stock in self.user_stocks:
+            # stock_start_date = stock.index[0].date()
+            # stock_end_date = stock.index[stock.shape[0]-1].date()
+            stock_start_date = "2022-01-01"
+            beta,_,_,_,_ = linregress(self.sp500['Log Returns'][stock_start_date:],stock['Log Returns'][stock_start_date:])
+            self.betas_for_portfolio.append(beta)
+
+        self.betas_for_portfolio = np.array(self.betas_for_portfolio)
+
+    def bootstrap(self):
+        data = np.random.choice(self.sp500['Log Returns'], size=630, replace=True)
+        return pd.DataFrame(data, columns=['Log Returns'])
+    
+    def simulate_sp500(self, bootstraped_sp500):
+        shock = self.user_shock * 2.5
+        daily_shock = (1 + shock) ** (1/630) - 1
+
+        for i in range (0,630):
+            bootstraped_sp500.iloc[i] += daily_shock
+
+    def calculate_returns(self, weights):
+        past_year_sp500_copy = self.bootstrap()
+        self.simulate_sp500(past_year_sp500_copy)
+        self.betas_for_portfolio = np.array(self.betas_for_portfolio).reshape(1, len(self.user_stocks_str))
+        past_year_sp500_copy = past_year_sp500_copy.values.reshape(630, 1)
+        portfolio_returns_after_shock = self.betas_for_portfolio * past_year_sp500_copy 
+        portfolio_returns_after_shock = pd.DataFrame(portfolio_returns_after_shock, columns=self.user_stocks_str)
+        portfolio_annual_return = (np.sum(portfolio_returns_after_shock.mean() * weights) * 252) - self.annual_risk_free_rate
+        return portfolio_annual_return, portfolio_returns_after_shock
+    
+    def calculate_volatility(self, weights, df_portfolio_returns_after_shock):
+        annualized_cov = np.dot(df_portfolio_returns_after_shock.cov()*252, weights)
+        vol = np.dot(weights.transpose(),annualized_cov)
+        return np.sqrt(vol)
+    
+    def generate_weights (self, n):
+        weights = np.random.random(n)
+        return weights / np.sum(weights)
+    
+    def simulate (self, n):
+        self.mc_port_returns = []
+        self.mc_port_vol = []
+        self.mc_port_weights = []
+        self.mc_sharpe_ratios
+
+        for i in range(n):
+            sim_weights = self.generate_weights(len(self.user_stocks_str))
+            self.mc_port_weights.append(sim_weights)
+            sim_returns, sim_port_after_shock = self.calculate_returns(sim_weights)
+            self.mc_port_returns.append(sim_returns)
+            sim_vol = self.calculate_volatility(sim_weights, sim_port_after_shock)
+            self.mc_port_vol.append(sim_vol)
+
+        self.mc_sharpe_ratios = np.array(self.mc_port_returns)/np.array(self.mc_port_vol)
+
+    def create_graph(self):
+        fig = plt.figure(dpi=200,figsize=(10,5))
+        plt.scatter(self.mc_port_vol,self.mc_port_returns,c=self.mc_sharpe_ratios)
+        plt.colorbar(label='SHARPE RATIO')
+        plt.xlabel('VOL')
+        plt.ylabel('RETURNS')
+        return fig
+
+
+
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# from my_app.classes.monte_carlo_class import MonteCarlo
 
 st.title("Monte Carlo Project")
 user_stocks = st.text_input("Enter stock codes with a space: ")
--- pages/3_Personal Finance Visualizer.py
+++ pages/3_Personal Finance Visualizer.py
@@ -3,10 +3,65 @@ import sys
 import os
 import io
 import pandas as pd
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import calendar
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+class MyData:
+    def __init__(self, sheet_id):
+        self.df = pd.read_csv(sheet_id, parse_dates=['Date'])
+        self.df["Month"] = self.df["Date"].dt.month_name()
+        self.df['Year'] = self.df["Date"].dt.year
+        self.months = list(calendar.month_name)[1:]
 
-from my_app.classes.personal_finance_visualizer_class import MyData
+    def print_df(self):
+        return self.df
+
+    def total_expenses_pie(self):
+        total_price = [self.df[self.df["Catergory"] == i]['Price'].sum() for i in self.df['Catergory'].unique()]
+        fig, ax = plt.subplots()
+        ax.pie(total_price, labels=self.df['Catergory'].unique(), autopct='%1.0f%%')
+        ax.set_title("Total Expenses Pie Chart")
+        return fig
+
+    def expenses_for_year(self, year):
+        month_totals = [self.df[(self.df['Month'] == month) & (self.df['Year'] == year)]['Price'].sum() for month in self.months]
+        fig, ax = plt.subplots(figsize=(15, 10), dpi=300)
+        ax.plot(self.months, month_totals, marker="o", linewidth=2)
+        ax.set_title(f"Expenses for {year}", fontsize=18)
+        ax.set_xlabel("Months", fontsize=13)
+        ax.set_ylabel("Price", fontsize=13)
+        for i in range(12):
+            if i > 0 and month_totals[i] > month_totals[i-1]:
+                ax.annotate(round(month_totals[i], 2), (self.months[i], month_totals[i]), xytext=(self.months[i], month_totals[i] + 12))
+            else:
+                ax.annotate(round(month_totals[i], 2), (self.months[i], month_totals[i]), xytext=(self.months[i], month_totals[i] - 15), horizontalalignment='right')
+        return fig
+
+    def catergorical_expenses_per_month_for_a_year(self, catergories, year):
+        fig, ax = plt.subplots(figsize=(15, 10))
+        bar_width = 0.2
+        x = range(len(self.months))
+        for i, catergory in enumerate(catergories):
+            monthly_catergory_totals = [self.df[(self.df['Month'] == month) & (self.df['Catergory'] == catergory) & (self.df['Year'] == year)]['Price'].sum() for month in self.months]
+            ax.bar([pos + i * bar_width for pos in x], monthly_catergory_totals, width=bar_width, label=catergory)
+        ax.legend(catergories, loc="upper right")
+        ax.set_xlabel("Months", fontsize=13)
+        ax.set_ylabel("Expenditures", fontsize=13)
+        ax.set_title("Categorical Expenditures per Month", fontsize=15)
+        ax.set_xticks([pos + len(self.df['Catergory'].unique()) * bar_width / 2 for pos in x])
+        ax.set_xticklabels(self.months)
+        return fig
+
+    def get_years(self):
+        return self.df['Year'].unique()
+
+    def unique_catergories(self):
+        return self.df['Catergory'].unique()
+
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# from my_app.classes.personal_finance_visualizer_class import MyData
 
 st.title("Expense Analysis App")
 
